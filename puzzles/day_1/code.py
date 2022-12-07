025f9d8ecc6af524b9a949a0f6c68109bd130de9
QuantificAid <37964284+datorus@users.noreply.github.com>
2022-12-07 10:44:06 +0100

    Solve part 2 day 1

--- puzzles/day_1/code.py
+++ puzzles/day_1/code.py
@@ -36,7 +36,12 @@ def main():
     print("elves", elves)
     total_calories = elves_total_calories(elves)
     print("total_calories", total_calories)
+
+    # Result part 1
     print("max_total_calories", max(total_calories))
+
+    # Result part 2
+    print(sum(sorted(total_calories)[-3:]))
 
 
 if __name__ == "__main__":
