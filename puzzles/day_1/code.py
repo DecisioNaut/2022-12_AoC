4efba4e1faa0666df04d0029e9ca0d45ec72ea2c
QuantificAid <37964284+datorus@users.noreply.github.com>
2022-12-07 10:41:47 +0100

    Solve part 1

---
+++ puzzles/day_1/code.py
@@ -0,0 +1,43 @@
+from typing import List
+
+1
+
+
+def read_elves(file: str) -> List[List[int]]:
+
+    elves = []
+    elf = []
+
+    with open("./puzzles/day_1/" + file) as in_file:
+        for item in in_file.readlines():
+            if item.strip() == "":
+                elves.append(elf)
+                elf = []
+            else:
+                elf.append(int(item))
+
+        elves.append(elf)
+
+    return elves
+
+
+def elves_total_calories(elves: List[List[int]]) -> List[int]:
+
+    total_calories = []
+
+    for elf in elves:
+        total_calories.append(sum(elf))
+
+    return total_calories
+
+
+def main():
+    elves = read_elves("part_1_data.txt")
+    print("elves", elves)
+    total_calories = elves_total_calories(elves)
+    print("total_calories", total_calories)
+    print("max_total_calories", max(total_calories))
+
+
+if __name__ == "__main__":
+    main()
