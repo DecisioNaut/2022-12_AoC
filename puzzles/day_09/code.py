2f91fabbba1974d27523329f0b27e4552e3163ee
QuantificAid <37964284+datorus@users.noreply.github.com>
2022-12-08 19:32:20 +0100

    Preparing day_09

---
+++ puzzles/day_09/code.py
@@ -0,0 +1,11 @@
+def get_data(file: str):
+    with open("./puzzles/day_09/" + file, mode="r") as in_file:
+        ...
+
+
+def main():
+    data = get_data("example.txt")
+
+
+if __name__ == "__main__":
+    main()
