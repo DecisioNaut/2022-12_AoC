8796ae547836f0788670f4917841bfc478e5f3ff
QuantificAid <37964284+datorus@users.noreply.github.com>
2022-12-09 15:25:44 +0100

    Solving day09 part 1

--- puzzles/day_09/code.py
+++ puzzles/day_09/code.py
@@ -1,10 +1,113 @@
+from __future__ import annotations
+
+from typing import Literal, List
+from pprint import pprint
+
+
 def get_data(file: str):
     with open("./puzzles/day_09/" + file, mode="r") as in_file:
-        ...
+        for line in in_file.readlines():
+            yield line.strip().split(" ")
+
+
+class Vector:
+    def __init__(self, x: int, y: int) -> None:
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other: Vector) -> bool:
+        return (self.x == other.x) & (self.y == other.y)
+
+    def __add__(self, other: Vector) -> Vector:
+        assert isinstance(other, Vector)
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+
+    def __sub__(self, other: Vector) -> Vector:
+        assert isinstance(other, Vector)
+        x = self.x - other.x
+        y = self.y - other.y
+        return Vector(x, y)
+
+    @property
+    def pull(self) -> Vector:
+        if (abs(self.x) <= 1) & (abs(self.y) <= 1):
+            return Vector(0, 0)
+        elif (abs(self.x) >= 2) & (abs(self.y) >= 2):
+            return AssertionError
+        else:
+
+            def create_pull(x_or_y: int) -> int:
+                if x_or_y == 0:
+                    return 0
+                elif x_or_y >= 1:
+                    return 1
+                elif x_or_y <= -1:
+                    return -1
+
+            pull_x = create_pull(self.x)
+            pull_y = create_pull(self.y)
+            return Vector(pull_x, pull_y)
+
+    def __str__(self) -> str:
+        return f"({self.x}, {self.y})"
+
+    __repr__ = __str__
+
+
+class MovingPart:
+    def __init__(self, x_start: int, y_start: int) -> None:
+        start_position = Vector(x_start, y_start)
+        self.position = start_position
+        self.history = [start_position]
+
+    def move_by_instruction(self, direction: Literal["R", "U", "D", "R"]) -> None:
+        match direction:
+            case "R":
+                move = Vector(1, 0)
+            case "U":
+                move = Vector(0, 1)
+            case "D":
+                move = Vector(0, -1)
+            case "L":
+                move = Vector(-1, 0)
+            case _:
+                raise ValueError
+        new_position = self.position + move
+        self.position = new_position
+        self.history.append(new_position)
+
+    def follow_by_position(self, position: Vector) -> None:
+        difference = position - self.position
+        pull = difference.pull
+        new_position = self.position + pull
+        self.position = new_position
+        self.history.append(new_position)
+
+    @property
+    def visited_positions_count(self):
+        visited_positions = []
+        for position in self.history:
+            if not (position in visited_positions):
+                visited_positions.append(position)
+        return len(visited_positions)
 
 
 def main():
-    data = get_data("example.txt")
+    directions = []
+    for data in get_data("data.txt"):
+        directions.append({"instruction": data[0], "repeat": int(data[1])})
+
+    head = MovingPart(0, 0)
+    tail = MovingPart(0, 0)
+
+    for direction in directions:
+        for i in range(direction["repeat"]):
+            head.move_by_instruction(direction["instruction"])
+            tail.follow_by_position(head.position)
+
+    print("Part 1:", tail.visited_positions_count)
 
 
 if __name__ == "__main__":
