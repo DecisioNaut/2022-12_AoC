a5ab860906a4d4d6e8c4f04bfe96f1fa950aff76
QuantificAid <37964284+datorus@users.noreply.github.com>
2022-12-15 11:59:10 +0100

    Amend code.py setup for day14, day15

--- puzzles/day_14/code.py
+++ puzzles/day_14/code.py
@@ -4,9 +4,22 @@ def get_data(file: str) -> str:
             yield line.strip()
 
 
-def main():
-    ...
+def part_1(file: str):
+    print(
+        "Part 1:",
+    )
+
+
+def part_2(file: str):
+    print(
+        "Part 2:",
+    )
+
+
+def main(file: str):
+    part_1(file)
+    part_2(file)
 
 
 if __name__ == "__main__":
-    main()
+    main("example.txt")
