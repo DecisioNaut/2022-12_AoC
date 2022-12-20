1dfd6f2bfb2ec2ad5ab75a8fd3597e94806687e0
QuantificAid <37964284+datorus@users.noreply.github.com>
2022-12-20 10:34:00 +0100

    Setting up all remaining days including data up to day20

---
+++ puzzles/day_25/code.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+from typing import List
+
+
+def get_data(file: str):
+    with open("./puzzles/day_/" + file, mode="r") as in_file:
+        for line in in_file.readlines():
+            yield line.strip()
+
+
+def part1(file: str) -> None:
+    data = get_data(file)
+    print(
+        "Part 1:",
+    )
+
+
+def part2(file: str) -> None:
+    data = get_data(file)
+    print(
+        "Part 2:",
+    )
+
+
+def main(file: str) -> None:
+
+    part1(file)
+    part2(file)
+
+
+if __name__ == "__main__":
+    main("example.txt")
