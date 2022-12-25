e50586c60d0214a7e32eeb694c4e774e93403c12
QuantificAid <37964284+datorus@users.noreply.github.com>
2022-12-25 19:23:50 +0100

    Solving day25 part1

--- puzzles/day_25/code.py
+++ puzzles/day_25/code.py
@@ -9,13 +9,57 @@ def get_data(file: str):
             yield line.strip()
 
 
+DECODE = {"2": 2, "1": 1, "0": 0, "-": -1, "=": -2}
+ENCODE = {2: "2", 1: "1", 0: "0", -1: "-", -2: "="}
+
+
+def decode(encoded_num: str) -> int:
+
+    num = 0
+    for i, elm in enumerate(list(reversed(list(encoded_num)))):
+        num += int(5**i) * DECODE[elm]
+
+    return num
+
+
+def encode(num: int):
+    base5_array = []
+    while num:
+        base5_array.append(num % 5)
+        num = num // 5
+
+    mod_base5_array = base5_array.copy()
+
+    for i in range(len(mod_base5_array) - 1):
+        if mod_base5_array[i] > 2:
+            mod_base5_array[i] -= 5
+            mod_base5_array[i + 1] += 1
+
+    if mod_base5_array[-1] > 2:
+        mod_base5_array[-1] -= 5
+        mod_base5_array.append(1)
+
+    encoded = ""
+    for elm in reversed(mod_base5_array):
+        encoded += ENCODE[elm]
+
+    return encoded
+
+
 def part1(file: str) -> None:
-    print(
-        "Part 1:",
-    )
+
+    total_decoded = 0
+
+    for data in get_data(file):
+        total_decoded += decode(data)
+
+    encoded = encode(total_decoded)
+
+    print("Part 1:", encoded)
 
 
 def part2(file: str) -> None:
+
     print(
         "Part 2:",
     )
@@ -28,4 +72,4 @@ def main(file: str) -> None:
 
 
 if __name__ == "__main__":
-    main("example.txt")
+    main("data.txt")
