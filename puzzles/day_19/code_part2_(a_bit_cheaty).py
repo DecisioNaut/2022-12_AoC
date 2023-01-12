d503c5a4ef04608cb7d0fc444ad9f7ba0ce9ec01
QuantificAid <37964284+datorus@users.noreply.github.com>
2023-01-12 10:16:18 +0100

    Amend day19

--- puzzles/day_19/code_part2_(a_bit_cheaty).py
+++ puzzles/day_19/code_part2_(a_bit_cheaty).py
@@ -1,30 +1,30 @@
-def dfs(blueprint, max_bots, bots, materials, t, cache):
+def dfs(blueprint, max_robots, robots, materials, t, cache):
 
     if t == 0:
         return materials[3]
 
-    key = tuple([*bots, *materials])
+    key = tuple([*robots, *materials])
     if key in cache:
         return cache[key]
 
-    maxval = materials[3] + bots[3] * t
+    maxval = materials[3] + robots[3] * t
 
-    for bot, recipe in enumerate(blueprint):
+    for robot, recipe in enumerate(blueprint):
 
-        if bot != 3 and bots[bot] >= max_bots[bot]:
+        if robot != 3 and robots[robot] >= max_robots[robot]:
             continue
 
         wait = 0
 
-        for material, amt in enumerate(recipe):
+        for material, amount in enumerate(recipe):
 
-            if amt == 0:
+            if amount == 0:
                 continue
 
-            if bots[material] == 0:
+            if robots[material] == 0:
                 break
 
-            wait = max(wait, -(-(amt - materials[material]) // bots[material]))
+            wait = max(wait, -(-(amount - materials[material]) // robots[material]))
 
         else:
 
@@ -32,21 +32,21 @@ def dfs(blueprint, max_bots, bots, materials, t, cache):
             if t_remain <= 0:
                 continue
 
-            next_materials = [x + y * (wait + 1) for x, y in zip(materials, bots)]
-            for material, amt in enumerate(recipe):
-                next_materials[material] -= amt
+            next_materials = [x + y * (wait + 1) for x, y in zip(materials, robots)]
+            for material, amount in enumerate(recipe):
+                next_materials[material] -= amount
                 next_materials[material] = min(
-                    next_materials[material], max_bots[material] * t_remain
+                    next_materials[material], max_robots[material] * t_remain
                 )
-            # for i in range(3):
-            #    next_materials[i] = min(next_materials[i], max_bots[i] * t_remain)
 
-            next_bots = bots[:]
-            next_bots[bot] += 1
+            next_robots = robots[:]
+            next_robots[robot] += 1
 
             maxval = max(
                 maxval,
-                dfs(blueprint, max_bots, next_bots, next_materials, t_remain, cache),
+                dfs(
+                    blueprint, max_robots, next_robots, next_materials, t_remain, cache
+                ),
             )
 
     cache[key] = maxval
@@ -55,7 +55,7 @@ def dfs(blueprint, max_bots, bots, materials, t, cache):
 
 
 blueprint = [[4, 0, 0], [2, 0, 0], [3, 14, 0], [2, 0, 7]]
-max_bots = [max(recipe[bot] for recipe in blueprint) for bot in range(3)]
-print(max_bots)
+max_robots = [max(recipe[robot] for recipe in blueprint) for robot in range(3)]
+print(max_robots)
 
-print(dfs(blueprint, max_bots, [1, 0, 0, 0], [0, 0, 0, 0], 24, {}))
+print(dfs(blueprint, max_robots, [1, 0, 0, 0], [0, 0, 0, 0], 24, {}))
